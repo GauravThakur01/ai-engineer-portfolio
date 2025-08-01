7fb13f30581b7552acb8a7e8fbc05515d85c125e
gauravthakur-itbd <gaurav.thakur@itbd.net>
2025-08-01 10:57:58 -0400

    Fix deployment issues

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"8zWN+cz4jtNUzCmDgdfQrvXRwnRZpYNIDM8vdFrYPrA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"K+fIZ7j2JyAOx7igyixHFdhcrs9IQTBZjrak62k02tg=\"}"
